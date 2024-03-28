7b7059bbede5abd7831f76964fd4f4087aa5fabc
hasferrr <79697518+hasferrr@users.noreply.github.com>
2024-03-28 09:18:25 +0700

    feat(types)!: remove ServerWithMembersWithUsers type definition

--- src/types/types.ts
+++
@@ -1,8 +0,0 @@
-import type { Channel, Member, Server, User } from '@prisma/client'
-
-export type ServerWithMembersWithUsers = Server & {
-  channels: Channel[]
-  members: (Member & {
-    user: User
-  })[]
-}
