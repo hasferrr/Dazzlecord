feb12c3b44036c37939ee7e67fd860a111a10816
hasferrr <hasferrr@proton.me>
2024-04-22 15:37:47 +0700

    fix: missing discord navigation line icon

--- src/components/navigation/navigation-home.tsx
+++ src/components/navigation/navigation-home.tsx
@@ -22,15 +22,15 @@ const NavigationHome = () => {
           'dark:bg-[var(--dark-page)]',
           'group-hover:rounded-[16px]',
           'group-hover:bg-indigo-500',
-          pathname === '/app' && 'rounded-[16px] bg-indigo-500 dark:bg-indigo-500'
+          pathname === '/channels/@me' && 'rounded-[16px] bg-indigo-500 dark:bg-indigo-500'
         )}>
           <div className={cn('absolute left-0 bg-primary rounded-r-[24px] transition-all w-[4px]',
-            pathname !== '/app' && 'group-hover:h-[20px]',
-            pathname === '/app' ? 'h-[36px]' : 'h-0',
+            pathname !== '/channels/@me' && 'group-hover:h-[20px]',
+            pathname === '/channels/@me' ? 'h-[36px]' : 'h-0',
           )} />
           <Image
             className={cn('group-hover:text-white transition dark:invert group-hover:invert',
-              pathname === '/app' && 'invert',
+              pathname === '/channels/@me' && 'invert',
             )}
             src="https://assets-global.website-files.com/6257adef93867e50d84d30e2/653714c17467993e7b389c83_636e0a6918e57475a843f59f_icon_clyde_black_RGB.svg"
             alt=""
