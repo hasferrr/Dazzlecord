73324e786a0dd3f422b1855daf84ff0e3552f241
hasferrr <hasferrr@proton.me>
2024-04-26 23:34:14 +0700

    fix: fix smol save button on chat items

--- src/components/chat/item/chat-item-edit-form.tsx
+++ src/components/chat/item/chat-item-edit-form.tsx
@@ -55,6 +55,10 @@ const ChatItemEditForm = ({
   }, [])
 
   const onSubmit = async (values: z.infer<typeof messageSchema>) => {
+    if (message.content === values.content) {
+      setIsEditing(false)
+      return
+    }
     setTransition(async () => {
       const updatedMessage = await editMessage(
         values,
@@ -73,53 +77,52 @@ const ChatItemEditForm = ({
 
   return (
     <Form {...form}>
-      <form
-        className="flex items-center w-full pt-2 gap-x-2"
-        onSubmit={form.handleSubmit(onSubmit)}
-      >
-        <FormField
-          control={form.control}
-          name="content"
-          render={({ field }) => (
-            <FormItem className="flex-1">
-              <FormControl>
-                <div className="relative w-full">
-                  <Input
-                    disabled={isPending}
-                    className="p-2 border-0 border-none bg-chat-input dark:bg-chat-input-dark
-                    focus-visible:ring-0 focus-visible:ring-offset-0 text-text-dark dark:text-text"
-                    placeholder="Edited message"
-                    {...field}
-                  />
-                </div>
-              </FormControl>
-            </FormItem>
-          )}
-        />
-        <Button disabled={isPending} size="sm" variant="primary">
-          Save
-        </Button>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <div className="flex items-center w-full pt-2 gap-x-2">
+          <FormField
+            control={form.control}
+            name="content"
+            render={({ field }) => (
+              <FormItem className="flex-1">
+                <FormControl>
+                  <div className="relative w-full">
+                    <Input
+                      disabled={isPending}
+                      className="p-2 border-0 border-none bg-chat-input dark:bg-chat-input-dark
+                      focus-visible:ring-0 focus-visible:ring-offset-0 text-text-dark dark:text-text"
+                      placeholder="Edited message"
+                      {...field}
+                    />
+                  </div>
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <Button disabled={isPending} size="sm" variant="primary">
+            Save
+          </Button>
+        </div>
+        <div className="text-[11px] mt-1">
+          Press escape to
+          {' '}
+          <button
+            disabled={isPending}
+            className="text-[#006ce7] dark:text-[#00a8fc] hover:underline"
+            onClick={() => setIsEditing(false)}
+          >
+            cancel
+          </button>
+          , enter to
+          {' '}
+          <button
+            disabled={isPending}
+            className="text-[#006ce7] dark:text-[#00a8fc] hover:underline"
+            type="submit"
+          >
+            save
+          </button>
+        </div>
       </form>
-      <div className="text-[11px] mt-1">
-        Press escape to
-        {' '}
-        <button
-          disabled={isPending}
-          className="text-[#006ce7] dark:text-[#00a8fc] hover:underline"
-          onClick={() => setIsEditing(false)}
-        >
-          cancel
-        </button>
-        , enter to
-        {' '}
-        <button
-          disabled={isPending}
-          className="text-[#006ce7] dark:text-[#00a8fc] hover:underline"
-          type="submit"
-        >
-          save
-        </button>
-      </div>
     </Form>
   )
 }
