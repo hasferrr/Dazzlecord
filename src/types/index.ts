c00eedcb5192643bf4eeeb77f5e2648b46de8e2b
hasferrr <hasferrr@proton.me>
2024-04-30 13:53:49 +0700

    add TODO (user no password)

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,6 +2,18 @@ import type {
   Channel, Friend, Member, Message, Server, User,
 } from '@prisma/client'
 
+// TODO: Omits ALL passwordHash from the codebase :)
+//
+// Add this to .eslintrj.json
+// "no-restricted-imports": ["error", {
+//   "paths": [{
+//     "name": "@prisma/client",
+//     "importNames": ["User"],
+//     "message": "Please import UserNoPassword instead."
+//   }]
+// }]
+export type UserNoPassword = Omit<User, 'passwordHash'>
+
 export type MessageWithUser = Message & { user: User }
 export type ServerWithChannel = Server & { channels: Channel[] }
 export type MemberWithUser = Member & { user: User }
