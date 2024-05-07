03761c7d932c8d669779fa4d866c01a9f6473dce
hasferrr <79697518+hasferrr@users.noreply.github.com>
2024-05-07 10:11:00 +0700

    refactor(chat-input): remove Form ui & use palin useForm hook

--- src/components/chat/chat-input.tsx
+++ src/components/chat/chat-input.tsx
@@ -4,17 +4,12 @@ import { useEffect, useRef } from 'react'
 
 import { zodResolver } from '@hookform/resolvers/zod'
 import type { DirectMessage, Message } from '@prisma/client'
-import { Plus, Smile } from 'lucide-react'
+import { Plus, SendHorizontal } from 'lucide-react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-} from '@/components/ui/form'
 import { Textarea } from '@/components/ui/textarea'
+import { cn } from '@/lib/utils'
 import { messageSchema } from '@/schemas/message-schema'
 
 interface ChatInputProps {
@@ -77,59 +72,51 @@ const ChatInput = ({
   }
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)}>
-        <FormField
-          control={form.control}
-          name="content"
-          render={() => (
-            <FormItem>
-              <FormControl>
-                <div className="relative pb-6">
-                  <button
-                    type="button"
-                    onClick={() => { }}
-                    className="absolute top-3 bottom-3 left-4 h-[24px] w-[24px]
-                    bg-zinc-500 dark:bg-zinc-300 hover:bg-zinc-600 dark:hover:bg-white
-                    transition rounded-full p-1 flex items-center justify-center"
-                  >
-                    <Plus className="text-white dark:text-[var(--dark-page)]" />
-                  </button>
-                  <Textarea
-                    {...form.register('content', {
-                      onChange: (e) => {
-                        e.target.style.height = 'auto'
-                        e.target.style.height = `${e.target.scrollHeight}px`
-                      },
-                    })}
-                    ref={(e) => {
-                      form.register('content').ref(e)
-                      // @ts-ignore
-                      textareaRef.current = e
-                    }}
-                    className="px-14 py-[14px] bg-chat-input dark:bg-chat-input-dark
-                    border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0
-                    min-h-[50px] max-h-[320px] overflow-y-auto resize-none"
-                    placeholder={`Message ${type === 'channel' ? '#' : '@'}${channelName}`}
-                    rows={1}
-                    id="chat-input"
-                    onKeyDown={(e) => {
-                      if (e.key === 'Enter' && !e.shiftKey) {
-                        e.preventDefault()
-                        form.handleSubmit(onSubmit)()
-                      }
-                    }}
-                  />
-                  <button className="absolute top-3 right-4">
-                    <Smile />
-                  </button>
-                </div>
-              </FormControl>
-            </FormItem>
+    <form onSubmit={form.handleSubmit(onSubmit)}>
+      <div className="relative pb-6">
+        <button
+          type="button"
+          onClick={() => { }}
+          className={cn(
+            'absolute top-3 bottom-3 left-4 h-[24px] w-[24px]',
+            'bg-zinc-500 dark:bg-zinc-300 hover:bg-zinc-600 dark:hover:bg-white',
+            'transition rounded-full p-1 flex items-center justify-center',
           )}
+        >
+          <Plus className="text-white dark:text-page-dark" />
+        </button>
+        <Textarea
+          {...form.register('content', {
+            onChange: (e) => {
+              e.target.style.height = 'auto'
+              e.target.style.height = `${e.target.scrollHeight}px`
+            },
+          })}
+          ref={(e) => {
+            form.register('content').ref(e)
+            // @ts-ignore
+            textareaRef.current = e
+          }}
+          className={cn(
+            'px-14 py-[14px] bg-chat-input dark:bg-chat-input-dark',
+            'border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0',
+            'min-h-[50px] max-h-[320px] overflow-y-auto resize-none',
+          )}
+          placeholder={`Message ${type === 'channel' ? '#' : '@'}${channelName}`}
+          rows={1}
+          id="chat-input"
+          onKeyDown={(e) => {
+            if (e.key === 'Enter' && !e.shiftKey) {
+              e.preventDefault()
+              form.handleSubmit(onSubmit)()
+            }
+          }}
         />
-      </form>
-    </Form>
+        <button type="submit" className="absolute top-3 right-4">
+          <SendHorizontal />
+        </button>
+      </div>
+    </form>
   )
 }
 
