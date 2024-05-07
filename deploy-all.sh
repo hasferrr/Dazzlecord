aa0be6bc71984c65eca9f3f1b87879fecc1c1e25
hasferrr <79697518+hasferrr@users.noreply.github.com>
2024-05-07 09:51:45 +0700

    ops: add deploy-all.sh

--- deploy-all.sh
+++ deploy-all.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# chmod +x deploy.sh
+# chmod +x deploya-all.sh
 
 gcloud auth configure-docker && \
+gcloud builds submit --config=cloudbuild-socket-bun.yaml & \
 gcloud builds submit --config=cloudbuild.yaml
