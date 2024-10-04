36005e31d6a73b27548e72f27155fbcbe58a3950
hasferrr <79697518+hasferrr@users.noreply.github.com>
2024-10-04 16:44:45 +0700

    feat: adjust cloud storage service account config & env reqs

--- src/lib/cloud-storage.ts
+++ src/lib/cloud-storage.ts
@@ -1,8 +1,25 @@
 import { Storage } from '@google-cloud/storage'
 
-import { GCS_PROJECT_ID, GCS_SERVICE_ACCOUNT_KEY_FILE } from '@/utils/config'
+import {
+  GCS_CRED_CLIENT_EMAIL,
+  GCS_CRED_PRIVATE_KEY,
+  GCS_PROJECT_ID,
+  GCS_SERVICE_ACCOUNT_KEY_FILE,
+} from '@/utils/config'
 
-export const storage = new Storage({
+const confFile = {
   projectId: GCS_PROJECT_ID,
   keyFilename: GCS_SERVICE_ACCOUNT_KEY_FILE,
-})
+}
+
+const confEnv = {
+  projectId: GCS_PROJECT_ID,
+  credentials: {
+    type: 'service_account',
+    project_id: GCS_PROJECT_ID,
+    private_key: GCS_CRED_PRIVATE_KEY,
+    client_email: GCS_CRED_CLIENT_EMAIL,
+  },
+}
+
+export const storage = new Storage(GCS_SERVICE_ACCOUNT_KEY_FILE ? confFile : confEnv)
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,19 @@
 /* eslint-disable no-process-env */
 
 const throwError = (envName: string): never => {
+  const envs = {
+    NODE_ENV: process.env.NODE_ENV,
+    DATABASE_URL: process.env['DATABASE_URL'],
+    AUTH_SECRET: process.env['AUTH_SECRET'],
+    GCS_PROJECT_ID: process.env['GCS_PROJECT_ID'],
+    GCS_BUCKET_NAME: process.env['GCS_BUCKET_NAME'],
+    ORIGIN_URL: process.env['ORIGIN_URL'],
+    NEXT_PUBLIC_SOCKET_IO_URL: process.env['NEXT_PUBLIC_SOCKET_IO_URL'],
+    GCS_SERVICE_ACCOUNT_KEY_FILE: process.env['GCS_SERVICE_ACCOUNT_KEY_FILE'],
+    GCS_CRED_PRIVATE_KEY: `${process.env['GCS_CRED_PRIVATE_KEY']?.slice(0, 100)}...`,
+    GCS_CRED_CLIENT_EMAIL: process.env['GCS_CRED_CLIENT_EMAIL'],
+  }
+  console.error(envs)
   throw new Error(`please specify all required environment variables! (missing: ${envName})`)
 }
 
@@ -15,7 +28,14 @@ export const NODE_ENV = getProcessEnv('NODE_ENV')
 export const DATABASE_URL = getProcessEnv('DATABASE_URL')
 export const AUTH_SECRET = getProcessEnv('AUTH_SECRET')
 export const GCS_PROJECT_ID = getProcessEnv('GCS_PROJECT_ID')
-export const GCS_SERVICE_ACCOUNT_KEY_FILE = getProcessEnv('GCS_SERVICE_ACCOUNT_KEY_FILE')
 export const GCS_BUCKET_NAME = getProcessEnv('GCS_BUCKET_NAME')
 export const ORIGIN_URL = parseUrl(getProcessEnv('ORIGIN_URL'))
 export const NEXT_PUBLIC_SOCKET_IO_URL = parseUrl(getProcessEnv('NEXT_PUBLIC_SOCKET_IO_URL'))
+
+export const { GCS_SERVICE_ACCOUNT_KEY_FILE } = process.env
+export const { GCS_CRED_PRIVATE_KEY } = process.env
+export const { GCS_CRED_CLIENT_EMAIL } = process.env
+
+if (!GCS_SERVICE_ACCOUNT_KEY_FILE && !(GCS_CRED_PRIVATE_KEY && GCS_CRED_CLIENT_EMAIL)) {
+  throwError('GCS Service Account Key')
+}
