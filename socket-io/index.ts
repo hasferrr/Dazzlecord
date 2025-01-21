3ef53a26465430e0c195971ec5aa350c49e20eea
hasferrr <79697518+hasferrr@users.noreply.github.com>
2025-01-21 10:09:11 +0700

    feat: update socket.io configuration to support multiple client URLs

--- socket-io/index.ts
+++ socket-io/index.ts
@@ -8,15 +8,23 @@ import messageRouter from './controllers/messageRouter'
 import { connectHandler } from './handlers/connect-handler'
 import { roomHandler } from './handlers/room-handler'
 
+const origin: string[] = []
+for (let i = 1; i <= 3; i += 1) {
+  const url = process.env[`CLIENT_URL_${i}`]
+  if (url) {
+    origin.push(url)
+  }
+}
+
 console.log(process.env.NODE_ENV)
-console.log('Allowed client: ', process.env['CLIENT_URL'])
+console.log('Allowed client: ', origin)
 
 const app = express()
 const server = createServer(app)
 
 const io = new Server(server, {
   cors: {
-    origin: process.env['CLIENT_URL'],
+    origin,
   },
   connectionStateRecovery: {},
 })
